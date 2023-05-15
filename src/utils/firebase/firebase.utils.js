475df83d73a2a55645ecbe65c5cd10214d1abd12
sohostory <sohostory@gmail.com>
2023-05-14 21:12:32 -0500

    added google signin function using firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
