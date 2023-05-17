f6002ae38936d3b7e3f94c5d513d372de88b45cd
sohostory <sohostory@gmail.com>
2023-05-16 22:10:20 -0500

    added sign up function

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -13,6 +18,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
