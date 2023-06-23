f669af890799fbfce150d0bbd396944eb5269f87
sohostory <sohostory@gmail.com>
2023-06-22 23:56:04 -0500

    added categories to firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,15 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useEffect, useState } from "react";
+
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = { products, setProducts };
+  const [products, setProducts] = useState([]);
+
+  const value = { products };
 
   return (
     <ProductsContext.Provider value={value}>
