1f56d9c55b821df8c15b044a4489b3a99d1c902c
sohostory <sohostory@gmail.com>
2023-06-27 21:16:57 -0500

    setting up redux store for user and categories

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import "./index.scss";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
+
 import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
 
@@ -17,13 +16,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
